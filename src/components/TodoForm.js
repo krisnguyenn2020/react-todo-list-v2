ee5532a9b1048d17743de73363796f2220d250a5
Kris Nguyen <krisnguyenn2020@gmail.com>
2020-11-17 21:31:09 -0500

    Add

---
+++ src/components/TodoForm.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect, useRef} from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState("");
+
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    })
+
+    const handleChange = event => {
+        setInput(event.target.value); 
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random()*1000),
+            text: input
+        });
+        setInput('');
+    };
+    return (
+        <div>
+            <form className = 'todo-form' onSubmit={handleSubmit}>
+                <input 
+                    type='text'
+                    placeholder = 'Add a todo' 
+                    value={input} 
+                    name='text' 
+                    className='todo-input'
+                    onChange= {handleChange}
+                    ref = {inputRef}    
+                 />
+                <button className='todo-button'>Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default TodoForm
